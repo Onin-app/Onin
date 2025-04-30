0d0ef027c6dadadc2a05bfac69ebd665401f30f2
baiyapeng <byapeng@qq.com>
2025-04-30 17:35:16 +0800

    feat: register global shortcut keys

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,9 +8,27 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let ctrl_n_shortcut = Shortcut::new(Some(Modifiers::CONTROL), Code::KeyN);
+
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::new()
+                .with_handler(move |app_handle, shortcut, event| {
+                    println!("Shortcut triggered: {:?} --> {:?}", shortcut, event.id);
+                })
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(move |app| {
+            #[cfg(desktop)]
+            {
+                println!("Registering Ctrl+N shortcut...");
+                app.global_shortcut().register(ctrl_n_shortcut)?;
+                println!("Registered!");
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
