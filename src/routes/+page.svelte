537404567c833942232d22a51c4c90334c34e1cc
baiyapeng <byapeng@qq.com>
2025-05-21 15:49:34 +0800

    feat: supported keyboard switching of selected apps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 
   let originAppList = $state<string[]>([]);
   let appList = $state<string[]>([]);
+  let selectedIndex = $state<number>(0);
 
   onMount(async () => {
     console.log("the component has mounted");
@@ -31,21 +32,57 @@
     const value = e.target.value;
     const apps = fuzzyMatch(value, originAppList);
     appList = apps;
+    selectedIndex = 0;
+  };
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "ArrowDown") {
+      e.preventDefault();
+      selectedIndex =
+        selectedIndex === appList.length - 1 ? 0 : selectedIndex + 1;
+    } else if (e.key === "ArrowUp") {
+      e.preventDefault();
+      selectedIndex =
+        selectedIndex === 0 ? appList.length - 1 : selectedIndex - 1;
+    }
+
+    // 保持选中项在可见范围内
+    const container = document.querySelector(".app-list");
+    const selectedItem = container?.children[selectedIndex];
+    if (selectedItem) {
+      selectedItem.scrollIntoView({
+        behavior: "auto",
+        block: "nearest",
+      });
+    }
   };
 </script>
 
-<main
-  class="w-full h-[100vh] p-4 rounded-xl bg-[aquamarine] overflow-hidden flex flex-col"
->
-  <input
-    class="w-full p-2 text-2xl h-[60px]"
-    type="text"
-    placeholder="Hi Baize!"
-    oninput={handleInput}
-  />
-  <div class="flex-1 py-2 overflow-auto">
-    {#each appList as app}
-      <div class="w-full p-2 text-2xl">{app}</div>
-    {/each}
+<main class="w-full h-[100vh] p-4 rounded-xl bg-[aquamarine] overflow-hidden">
+  <div
+    class="w-full h-full flex flex-col"
+    role="listbox"
+    tabindex="0"
+    onkeydown={handleKeyDown}
+  >
+    <input
+      class="w-full p-2 text-2xl h-[60px]"
+      type="text"
+      placeholder="Hi Baize!"
+      oninput={handleInput}
+    />
+    <div class="app-list flex-1 py-2 overflow-auto">
+      {#each appList as app, index}
+        <div
+          role="option"
+          aria-selected={selectedIndex === index}
+          class="w-full p-2 text-2xl {selectedIndex === index
+            ? 'bg-[aqua]'
+            : ''}"
+        >
+          {app}
+        </div>
+      {/each}
+    </div>
   </div>
 </main>
