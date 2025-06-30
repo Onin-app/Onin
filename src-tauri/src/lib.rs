5e27de4dd2828dae9f1928fc885ad79224abe30f
baiyapeng <byapeng@qq.com>
2025-06-30 14:28:04 +0800

    feat: added a tray icon and implemented opening and exiting the app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,9 @@
 use std::sync::atomic::{AtomicBool, Ordering};
-use tauri::{Emitter, Manager, State};
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    Emitter, Manager, State,
+};
 use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
 
 use tracing_subscriber;
@@ -146,6 +150,42 @@ pub fn run() {
                         _ => {}
                     }
                 });
+
+                // 创建托盘图标
+                let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+                let menu = Menu::with_items(app, &[&quit_i])?;
+                let tray = TrayIconBuilder::new()
+                    .icon(app.default_window_icon().unwrap().clone())
+                    .menu(&menu)
+                    .show_menu_on_left_click(true)
+                    .on_tray_icon_event(|tray, event| match event {
+                        TrayIconEvent::Click {
+                            button: MouseButton::Left,
+                            button_state: MouseButtonState::Up,
+                            ..
+                        } => {
+                            println!("left click pressed and released");
+                            // in this example, let's show and focus the main window when the tray is clicked
+                            let app = tray.app_handle();
+                            if let Some(window) = app.get_webview_window("main") {
+                                let _ = window.show();
+                                let _ = window.set_focus();
+                            }
+                        }
+                        _ => {
+                            println!("unhandled event {event:?}");
+                        }
+                    })
+                    .on_menu_event(|app, event| match event.id.as_ref() {
+                        "quit" => {
+                            println!("quit menu item was clicked");
+                            app.exit(0);
+                        }
+                        _ => {
+                            println!("menu item {:?} not handled", event.id);
+                        }
+                    })
+                    .build(app)?;
             }
             Ok(())
         })
